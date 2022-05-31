20dcaf4947b357a152ea1b717cef0a43b0527c46
Dominic Tremblay <dominictremblay24@gmail.com>
2022-05-31 12:16:07 -0400

    solving the sum problem

--- sum.js
+++ sum.js
@@ -1,13 +1,18 @@
 // - Write a node program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them. If any argument is not a whole number, skip it. Do support negative numbers though. If any argument is not a number, output an error message. We need at least 2 arguments.
 
-
 // create a function that goes through the steps.
 
 // grab the numbers from the command-line (command-line arguments)
 // process.argv
 const args = process.argv.slice(2);
+// We need at least 2 arguments
 
-console.log("args:", args);
+if (args.length < 2) {
+  console.log(`Error: please enter at least 2 arguments`);
+  process.exit();
+}
+
+console.log('args:', args);
 
 //  goes through each
 
@@ -21,17 +26,24 @@ let total = 0;
 
 for (let nb of args) {
   // sum them up
-  total += Number(nb);
-  console.log("nb:", nb, "total:", total);
+  //  edge case: If any argument is not a whole number, skip it
+  //  edge case: If any argument is not a number, output an error message.
+  
+  if (isNaN(Number(nb))) {
+    console.log(`Error: please enter only numbers`);
+    // break or return
+    // return => returning a value from a function => exit the function
+    process.exit()
+  }
+  
+  if (Number.isInteger(Number(nb))) {
+    total += Number(nb);
+  }
+
+
+  console.log('nb:', nb, 'total:', total);
 }
 
 
-
-
-//  edge case: If any argument is not a whole number, skip it
-//  edge case: If any argument is not a number, output an error message.
-
-
-
 // print the sum
-console.log("Total:", total)
+console.log('Total:', total);
