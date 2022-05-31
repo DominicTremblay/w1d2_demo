a06f97bc5ee3ed515a2e3069b7a89d77f2da7e88
Dominic Tremblay <dominictremblay24@gmail.com>
2022-05-31 11:34:23 -0400

    creating the list of steps

--- sum.js
+++ sum.js
@@ -1,1 +1,32 @@
 // - Write a node program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them. If any argument is not a whole number, skip it. Do support negative numbers though. If any argument is not a number, output an error message. We need at least 2 arguments.
+
+
+// create a function that goes through the steps.
+
+// grab the numbers from the command-line (command-line arguments)
+// process.argv
+const args = process.argv.slice(2);
+
+console.log("args:", args);
+
+//  goes through each
+
+// c-style
+// for of
+// for in
+// for each
+
+for (let nb of args) {
+
+}
+
+
+
+
+//  edge case: If any argument is not a whole number, skip it
+//  edge case: If any argument is not a number, output an error message.
+
+
+// sum them up
+
+// print the sum
